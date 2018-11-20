497e0bacea5100f97d2a6e133ee55da0e90468ac
briknthewall <blbracher@gmail.com>
2018-11-20 09:59:38 -0500

    initial commit

---
+++ RestaurantRater_MVC/Controllers/RestaurantController.cs
@@ -0,0 +1,25 @@
+﻿using RestaurantRater_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater_MVC.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDBContext db = new RestaurantDBContext();
+
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View(db.Restaurants.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
